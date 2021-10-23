45502b8d05ca306e9d2b17ea2e89adc317d16f08
okaabe <okaabe2006@gmail.com>
2021-10-23 15:49:46 -0300

    Removed assets folder

--- src/themes/kiss.lua
+++ src/themes/kiss.lua
@@ -34,32 +34,17 @@ theme.font_notifs       = "PragmataProMonoLiga Nerd Font 11"
 theme.font_hotkeys      = "PragmataProMonoLiga Nerd Font 11"
 theme.font_hotkeys_bold = "PragmataProMonoLiga Nerd Font 11"
 
--- Wibar Settigs
-theme.wibar_height = dpi(40)
-theme.wibar_border_width = dpi(2)
-theme.wibar_border_color = theme.xbgdark
-
 -- Wibox Background
 theme.bg_normal     = theme.xbg 
 theme.bg_focus      = theme.xbgdark
 theme.bg_urgent     = "#79695a"
 theme.bg_minimize   = "#201e1a"
 
--- Systray
-theme.systray_icon_spacing = dpi(5)
-theme.bg_systray    = theme.bg_focus
-
 -- Wibox Foreground
 theme.fg_normal     = theme.xcolor7
 theme.fg_focus      = theme.xcolor15
 theme.fg_urgent     = theme.xcolor2
 theme.fg_minimize   = theme.xcolor8
-
--- Widget
-theme.widget_bg = theme.xbgdark
-theme.widget_margin = dpi(5)
-theme.widget_padding = dpi(0)
-theme.widget_radius = dpi(12)
 
 -- Windows
 theme.border_width      = dpi(0)
@@ -108,38 +93,11 @@ theme.tabbar_style        = "i3"
 theme.tabbar_size         = dpi(28)
 theme.tabbar_ontop        = true
 theme.tabbar_position     = "top"
---[[ Deprecated
-theme.tabbar_color_close = "#674441"
-theme.tabbar_color_min = "#84694e"
-theme.tabbar_color_float = "#545e5e"
---]]
 theme.tabbar_bg_normal = theme.xbg
 theme.tabbar_fg_normal = theme.fg_normal
 theme.tabbar_bg_focus  = theme.xcolor4
 theme.tabbar_fg_focus  = theme.xfg
 theme.tabbar_bg_focus_inactive = theme.xbglight
-
-
--- Bling mstab settings
-theme.mstab_border_radius = dpi(0)
---[[
-theme.mstab_bar_height      = dpi(40)
-theme.mstab_font            = theme.font
-]]
-theme.mstab_bar_padding     = dpi(0)
---theme.mstab_tabbar_position = "right"
---[[theme.mstab_tabbar_style    = "boxes"
-theme.mstab_tabbar_ontop    = true
-theme.mstab_bg_focus        = theme.bg_focus
-theme.mstab_fg_focus        = theme.fg_focus
-theme.mstab_bg_normal       = theme.bg_normal
-theme.mstab_fg_normal       = theme.fg_normal
---]]
-
--- Taglist
-theme.taglist_fg_focus = theme.xcolor4
-theme.taglist_fg_empty = theme.xcolor8
-theme.taglist_fg_occupied = theme.xcolor2
 
 -- Window switcher
 theme.window_switcher_widget_bg = theme.xbg
@@ -157,55 +115,7 @@ theme.titlebar_fg_normal = theme.xcolor15
 theme.titlebar_height    = dpi(28)
 theme.titlebar_margin = dpi(10)
 theme.titlebar_spacing = dpi(3)
-theme.titlebar_border_width = theme.border_width
-theme.titlebar_border_color = theme.tborder_normal
 
-local assets =  gears.filesystem.get_configuration_dir() .. "/assets/"
-
-theme.titlebar_close_button_normal = assets .. 'titlebar/blocks/close_normal.png'
-theme.titlebar_close_button_focus  = assets .. 'titlebar/blocks/close_focus.png'
-theme.titlebar_close_button_normal_hover = assets .. 'titlebar/blocks/close_normal_hover.png'
-theme.titlebar_close_button_focus_hover  = assets .. 'titlebar/blocks/close_focus_hover.png'
-
-theme.titlebar_minimize_button_normal = assets .. "titlebar/blocks/minimize_normal.png"
-theme.titlebar_minimize_button_focus  = assets .. "titlebar/blocks/minimize_focus.png"
-theme.titlebar_minimize_button_normal_hover = assets .. "titlebar/blocks/minimize_normal_hover.png"
-theme.titlebar_minimize_button_focus_hover  = assets .. "titlebar/blocks/minimize_focus_hover.png"
-
-theme.titlebar_maximized_button_normal_inactive = assets .. "titlebar/blocks/maximized_normal.png"
-theme.titlebar_maximized_button_focus_inactive  = assets .. "titlebar/blocks/maximize_focus.png"
-theme.titlebar_maximized_button_normal_active = assets .. "titlebar/blocks/maximized_normal.png"
-theme.titlebar_maximized_button_focus_active  = assets .. "titlebar/blocks/maximize_focus.png"
-theme.titlebar_maximized_button_normal_inactive_hover = assets .. "titlebar/blocks/maximize_normal_hover.png"
-theme.titlebar_maximized_button_focus_inactive_hover  = assets .. "titlebar/blocks/maximize_focus_hover.png"
-theme.titlebar_maximized_button_normal_active_hover = assets .. "titlebar/blocks/maximize_normal_hover.png"
-theme.titlebar_maximized_button_focus_active_hover  = assets .. "titlebar/blocks/maximize_focus_hover.png"
-
-theme.titlebar_ontop_button_normal_inactive = assets .. "titlebar/blocks/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = assets .. "titlebar/blocks/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = assets .. "titlebar/blocks/ontop_normal_active.png"
-theme.titlebar_ontop_button_normal_inactive_hover = assets .. "titlebar/blocks/ontop_normal_inactive_hover.png"
-theme.titlebar_ontop_button_focus_inactive_hover  = assets .. "titlebar/blocks/ontop_focus_inactive_hover.png"
-theme.titlebar_ontop_button_normal_active_hover = assets .. "titlebar/blocks/ontop_normal_active_hover.png"
-theme.titlebar_ontop_button_focus_active_hover  = assets .. "titlebar/blocks/ontop_focus_active_hover.png"
-
-theme.titlebar_floating_button_normal_inactive = assets .. "titlebar/blocks/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = assets .. "titlebar/blocks/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = assets .. "titlebar/blocks/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = assets .. "titlebar/blocks/floating_focus_active.png"
-
-theme.titlebar_floating_button_normal_inactive_hover = assets .. "titlebar/blocks/floating_normal_inactive_hover.png"
-theme.titlebar_floating_button_focus_inactive_hover  = assets .. "titlebar/blocks/floating_focus_inactive_hover.png"
-theme.titlebar_floating_button_normal_active_hover = assets .. "titlebar/blocks/floating_normal_active_hover.png"
-theme.titlebar_floating_button_focus_active_hover  = assets .. "titlebar/blocks/floating_focus_active_hover.png"
-
--- Awesome Icon
-theme.awesome_icon = assets .. "ghosts/awesome.png"
-theme.notif_icon   = assets .. "ghosts/notif.png"
-
-theme.layout_max = assets .. "layout/max.png"
-theme.layout_tile = assets .. "layout/tile.png"
-theme.layout_floating = assets .. "layout/floating.png"
 return theme
 
 
