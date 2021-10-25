8726a40bdd4f78c7adc0224c933122763bdb0fc6
okaabe <okaabe2006@gmail.com>
2021-10-25 10:10:22 -0300

    Removed useless rows

--- src/components/popups/menubar.lua
+++ src/components/popups/menubar.lua
@@ -9,7 +9,6 @@ require("awful.hotkeys_popup.keys")
 -- apps menu
 discord = "Discord"
 telegram = "telegram-desktop"
-spotify = "spotify"
 browser = "qutebrowser"
 appsmenu = "rofi -show drun"
 
@@ -18,47 +17,38 @@ shoot = "flameshot gui"
 editor = "nvim"
 terminal = "alacritty"
 editor_cmd = terminal .. " -e " .. editor
-virtmgr = "virt-manager"
-zoom = gfs.get_configuration_dir() .. "boomer"
+zoom = gfs.get_configuration_dir() .. "libs/zoom"
 browser = "firefox"
 
 -- system menu
-switch = "home-manager switch"
-reboot = "doas reboot"
-shutdown = "doas shutdown now"
+reboot = "reboot"
+shutdown = "poweroff"
 
 -- apps table
 apps = {
     { "DISCORD", discord },
-    --{ "TELEGRAM", telegram },
-    { "SPOTIFY", spotify },
     { "BROWSER", browser },
-    --{ "MORE",    appsmenu },
 }
 
 -- tools table
 tools = {
     { "SCREENSHOT", shoot },
     { "EDITOR", editor_cmd },
-    { "VIRTMGR", virtmgr },
     { "TERMINAL", terminal },
     { "ZOOM", zoom }
 }
 
 -- system table
 system = {
-    { "SWITCH", switch },
     { "REBOOT", reboot },
-    { "SHUTDOWN", shutdown }
+    { "SHUTDOWN", shutdown },
+    { "EXIT", function() awesome.quit() end }
 }
 
 --- table of awesome
 awesomemenu = {
     { "HOTKEYS", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
-    { "MANUAL", terminal .. " -e man awesome" },
-    { "EDIT CONFIG", editor_cmd .. " " .. awesome.conffile },
     { "RESTART", function() awesome.restart() end },
-    { "EXIT", function() awesome.quit() end }
 }
 
 
@@ -73,10 +63,8 @@ mymainmenu = awful.menu({
 
 
 mylauncher = awful.widget.launcher({
-    image = beautiful.awesome_icon,
     menu = mymainmenu
 })
-
 
 -- Menubar configuration
 menubar.utils.terminal = terminal
